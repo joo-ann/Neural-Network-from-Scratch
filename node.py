887d8249a8d4e91b797608984b0884e24027869a
joo-ann <juanbonifasizhang@gmail.com>
2026-04-22 20:14:50 -0600

    meow

--- node.py
+++ node.py
@@ -1,13 +1,18 @@
 import numpy as np
+import random
+import math
 
 class Node():
-    def __init__(self, weight, bias, n, p, v=-1):
-        self.weight = weight
-        self.bias = bias
-        self.value = v
-        self.next = n
+    def __init__(self, w=[], b=[], p=[], v=-1):
         self.prev = p
+        self.weights = w
+        self.bias = b
+        self.value = v
        
+    
+    def sigmoid(x):
+        return 1/(1+(math.e)^(-x))
+    
 
     def run(self, inp):
         if self.prev == []:
@@ -21,13 +26,74 @@ class Node():
             
             self.value = sum(calc)
 
-    def add_next(self, *node):
-        self.next.append(i for i in node)
-
     def add_prev(self, *node):
         self.prev.append(i for i in node)
 
     def get_value(self):
         return self.value
+    
+    def get_weights(self):
+        return self.weights
+    
+    def get_biases(self):
+        return self.bias
 
 
+class Network():
+    def __init__(self, *layers):
+        self.network = []
+        self.layers = []
+        self.biases = []
+        self.network.append([Node() for _ in range(layers[0])])
+        for i in range(1, len(layers)):
+            l = []
+            for j in range(layers[i]): 
+                w = [random.randint(-10, 10) for _ in range(layers[i-1])]
+                b = random.randint(-10, 10)
+                b = np.array(b)
+                p = self.network[i-1]
+                l.append(Node(w, b, p))
+            x = []
+            y = []
+            for i in l:
+                x.append(i.get_weights())
+                y.append(i.get_biases())
+                
+
+            self.layers.append(np.array(x))
+            self.biases.append(np.array(y).reshape(-1, 1))
+            self.network.append(l)
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def forward(self, inp):
+        v = inp
+        for i in range(len(self.layers)):
+            v = np.matmul(self.layers[i], v) + self.biases[i]
+            v = self.sigmoid(v)
+            print('v:', v)
+
+        print('Output:', v)
+
+    def get_weights(self):
+        return self.layers
+
+    def set_weights(self, l):
+        self.layers = l
+
+    def display(self): 
+        visual = []
+        for i in range(len(self.network)):
+            l = []
+            for _ in range(len(self.network[i])):
+                l.append('o')
+            visual.append(l)
+            
+        print(*visual, sep='\n')
+            
+test = Network(2, 2, 1)
+
+print("Weights:", test.get_weights())
+
+test.forward(np.array([[1], [1]]))
